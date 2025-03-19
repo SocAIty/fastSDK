67979c0938eca1bb56052ec6f51434c8d0b3181d
w4hns1nn <myballs@dontwanttoshare.com>
2025-03-19 15:21:53 +0100

    added basic api key error

--- fastsdk/web/req/request_handler_socaity.py
+++ fastsdk/web/req/request_handler_socaity.py
@@ -4,11 +4,30 @@ from fastsdk.web.definitions.endpoint import EndPoint
 from fastsdk.web.req.request_handler import RequestHandler
 from media_toolkit import MediaFile
 
+class APIKeyError(Exception):
+    """Custom exception for API key validation errors."""
+    def __init__(self, message: str):
+        message = f"{message}\nPlease create an account at https://www.socaity.ai/ and get an API key."
+
+        super().__init__(message)
+
 
 class RequestHandlerSocaity(RequestHandler):
     """
     Works with Socaity API: https://api.socaity.ai/docs
     """
+    def validate_api_key(self):
+        # pass for non officially hosted or localhost services
+        if "api.socaity.ai" not in self.service_address:
+            return True
+
+        if self.api_key is None:
+            raise APIKeyError("API key is required for Socaity API.")
+
+        if len(self.api_key) != 67 or not self.api_key.startswith("sk_"):
+            raise APIKeyError("Invalid API key. It should look like 'sk_...'. ")
+        return True
+
     async def _request_endpoint(
             self,
             url: Union[str, None],
@@ -19,6 +38,8 @@ class RequestHandlerSocaity(RequestHandler):
             timeout: float,
             endpoint: EndPoint
     ):
+        self.validate_api_key()
+
         # Socaity expects all parameters except of files as query parameters.
         # Only files go in the body.
         url_files = {k: v for k, v in file_params.items() if MediaFile._is_url(v)}
