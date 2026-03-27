a8a61f9dda9ae472bb48c6c6afb126d61897738e
w4hns1nn <myballs@dontwanttoshare.com>
2026-03-27 15:33:04 +0100

    refactored url parsing.. it is ready for testing

--- fastsdk/service_interaction/request/api_client_socaity.py
+++ fastsdk/service_interaction/request/api_client_socaity.py
@@ -1,37 +1,28 @@
-from .api_client import APIClient, APIKeyError
+from .api_client import APIClient, APIKeyError, RequestData
 from apipod_registry.definitions.service_definitions import SocaityServiceAddress
 import httpx
-from fastsdk.service_interaction.request.api_client import RequestData
 
-    
 class APIClientSocaity(APIClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.poll_method = "GET"
+
     def validate_api_key(self) -> bool:
-        if not isinstance(self.service_def.service_address, SocaityServiceAddress) or "api.socaity.ai" not in self.service_address.url:
+        service_address = self.service_def.service_address
+        if not isinstance(service_address, SocaityServiceAddress) or "api.socaity.ai" not in service_address.base_url:
             return True
         if self.api_key is None:
             raise APIKeyError("API key is required for Socaity API.", "socaity", "https://www.socaity.ai/")
-        if len(self.api_key) < 67 or not self.api_key.startswith("sk_") or not self.api_key.startswith("tk_"):
-            raise APIKeyError("Invalid API key. It should look like 'sk_...'. ", "socaity", "https://www.socaity.ai/")
+        if not self.api_key.startswith(("sk_", "tk_")) or len(self.api_key) < 10:
+            raise APIKeyError("Invalid API key. It should look like 'sk_...' or 'tk_...'.", "socaity", "https://www.socaity.ai/")
         return True
 
     async def send_request(self, request_data: RequestData, timeout_s: float = 60) -> httpx.Response:
-        """
-        Send the prepared request to the API.
-
-        Args:
-            service_def: Service definition
-            request_data: The prepared request data
-            timeout_s: Request timeout in seconds
-
-        Returns:
-            The API response
-        """
-        # Socaity expects data as multipart/form-data. Thus parameters are put into data and files.
-        # print(f"Sending socaity request to {request_data.url} with query params {request_data.query_params}, body params {request_data.body_params}, and file params {request_data.file_params}")
+        """Socaity expects multipart/form-data, so parameters go into data and files."""
         return await self.client.post(
             url=request_data.url,
             params=request_data.query_params,
-            json=request_data.body_params,
+            data=request_data.body_params,
             files=request_data.file_params,
             headers=request_data.headers,
             timeout=timeout_s
